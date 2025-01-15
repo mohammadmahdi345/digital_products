9d4285e3f3f8bdc91b6459fe2cac08f4ebc93754
mohammadmahdi <m99957543@gmail.com>
2025-01-15 17:37:51 +0330

    products models added.

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -1,3 +1,5 @@
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+
 """
 Django settings for digital_products project.
 
@@ -9,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# media fiels
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media/')
+MEDIA_URL = '/Media/'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
